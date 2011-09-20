fb55782b6f0d86c340e99d8f8e94d99dbbb4c165
kmachulis <kmachulis@kmachulis-mozilla-linux.(none)>
2011-09-19 17:24:01 -0700

    Created hello world project for cross compiling binaries for android arm via cmake

---
+++ android_env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# http://opekar.blogspot.com/2011/06/android-cmake-is-much-easier-in-ndk-r5b.html
+
+export NDK=/home/kmachulis/mozbuild/android-ndk-r6b/
+SYSROOT=$NDK/platforms/android-9/arch-arm
+
+MIDDLE=toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/
+PREF=arm-linux-androideabi-
+
+export CC="$NDK/$MIDDLE/${PREF}gcc  --sysroot=$SYSROOT"
+export CXX="$NDK/$MIDDLE/${PREF}g++  --sysroot=$SYSROOT"
+export LD="$NDK/$MIDDLE/${PREF}ld  --sysroot=$SYSROOT"
+export CPP="$NDK/$MIDDLE/${PREF}cpp  --sysroot=$SYSROOT"
+export AS="$NDK/$MIDDLE/${PREF}as  --sysroot=$SYSROOT"
+export OBJCOPY="$NDK/$MIDDLE/${PREF}objcopy  --sysroot=$SYSROOT"
+export OBJDUMP="$NDK/$MIDDLE/${PREF}objdump  --sysroot=$SYSROOT"
+export STRIP="$NDK/$MIDDLE/${PREF}strip  --sysroot=$SYSROOT"
+export RANLIB="$NDK/$MIDDLE/${PREF}ranlib  --sysroot=$SYSROOT"
+export CCLD="$NDK/$MIDDLE/${PREF}gcc  --sysroot=$SYSROOT"
+export AR="$NDK/$MIDDLE/${PREF}ar  --sysroot=$SYSROOT"
