311a808ab57ed1f6db1b0881d2d264cca2025a07
Fernando Jiménez <ferjmoreno@gmail.com>
2011-10-05 18:27:33 +0200

    Added speaker on/off examples

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,31 +1,54 @@
 #include <utils/Log.h>
 #include <media/AudioSystem.h>
 
-#define LOG_TAG "B2G-MICROPHONE-TEST"
+#ifndef LOG_TAG
+#define LOG_TAG "B2G-AUDIO-TEST"
+#endif
 
 using namespace android;
 
 int main(int argc, char **argv) {
+  if(argc <= 1) {
+    printf("Usage: ./b2g-audio-test <mic|speaker>");
+    return -1;
+  }
 
   status_t status;
   bool state = false;
 
-  status = AudioSystem::isMicrophoneMuted(&state);
-  LOGI("AudioSystem::isMicrophoneMuted() = %s.\n", state ? "true" : "false");
+  if(strcmp(argv[1],"mic")) {
+    status = AudioSystem::isMicrophoneMuted(&state);
+    LOGI("AudioSystem::isMicrophoneMuted() = %s.\n", state ? "true" : "false");
 
-  if(state){
-    status = AudioSystem::setMode(AudioSystem::MODE_IN_CALL);
-    LOGI("AudioSystem::setMode(AudioSystem::MODE_IN_CALL) = %d.\n", status);
-  }else{
-    status = AudioSystem::setMode(AudioSystem::MODE_IN_COMMUNICATION);
-    LOGI("AudioSystem::setMode(AudioSystem::MODE_IN_COMMUNICATION) = %d.\n", status);
+    if(state){
+      /**
+      * setMode is called when the audio mode changes. NORMAL mode is for
+      * standard audio playback, RINGTONE when a ringtone is playing and IN_CALL
+   	  * when a call is in progress.
+  	  */
+      status = AudioSystem::setMode(AudioSystem::MODE_IN_CALL);
+      LOGI("AudioSystem::setMode(AudioSystem::MODE_IN_CALL) = %d.\n", status);
+    }	else{
+      status = AudioSystem::setMode(AudioSystem::MODE_IN_COMMUNICATION);
+      LOGI("AudioSystem::setMode(AudioSystem::MODE_IN_COMMUNICATION) = %d.\n", status);
+    }
+
+    status = AudioSystem::muteMicrophone(!state);
+    LOGI("AudioSystem::muteMicrophone(%s) = %d.\n", !state ? "true" : "false", status);
+
+    status = AudioSystem::isMicrophoneMuted(&state);
+    LOGI("AudioSystem::isMicrophoneMuted() = %s.\n", state ? "true" : "false");
+    
+  } else if (strcmp(argv[1],"speaker")) {
+    status = AudioSystem::setMode(AudioSystem::MODE_RINGTONE); 
+    LOGI("AudioSystem::setMode(AudioSystem::MODE_RINGTONE) = %d.\n", status);
+    status = AudioSystem::getMasterMute(&state);
+    LOGI("AudioSystem::getMasterMute() = %d. %s\n", status, state? "true":"false");
+    status = AudioSystem::setMasterMute(!state);
+    LOGI("AudioSystem::setMasterMute() = %d. %s\n", status);    
+    status = AudioSystem::getMasterMute(&state);
+    LOGI("AudioSystem::getMasterMute() = %d. %s\n", status, state? "true":"false"); 
   }
-
-  status = AudioSystem::muteMicrophone(!state);
-  LOGI("AudioSystem::muteMicrophone(%s) = %d.\n", !state ? "true" : "false", status);
-  
-  status = AudioSystem::isMicrophoneMuted(&state);
-  LOGI("AudioSystem::isMicrophoneMuted() = %s.\n", state ? "true" : "false");
 
   return 0;
 }
