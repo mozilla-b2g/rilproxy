9a8bc527d2669df6423acfdf1290a6b1cf7760a1
Kyle <kyle@nonpolynomial.com>
2011-11-08 21:56:19 -0800

    Made android makefile copy bin to correct directory in build tree

--- Android.mk
+++ Android.mk
@@ -2,13 +2,10 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES:= src/b2g-dialer-daemon.c
-
 LOCAL_SHARED_LIBRARIES := libcutils liblog
-
 LOCAL_MODULE:= b2g-dialer-daemon
-
-LOCAL_MODULE_TAGS:= optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
 
 include $(BUILD_EXECUTABLE)
