44f318bed4c44d37802d447a55bc30bd26c328c8
Kyle <kyle@nonpolynomial.com>
2011-11-09 17:50:02 -0800

    Adding commented out b2g-dialer-forward block to android makefile, just in case we need it again someday.

--- Android.mk
+++ Android.mk
@@ -7,5 +7,11 @@ LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE:= b2g-dialer-daemon
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng
-
 include $(BUILD_EXECUTABLE)
+# include $(CLEAR_VARS)
+# LOCAL_SRC_FILES:= src/b2g-dialer-forward.cpp
+# LOCAL_SHARED_LIBRARIES := libcutils liblog libmedia libutils libbinder
+# LOCAL_MODULE:= b2g-dialer-forward
+# LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+# LOCAL_MODULE_TAGS := eng
+# include $(BUILD_EXECUTABLE)
