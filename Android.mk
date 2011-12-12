2a8e45918ef35a7d7181bfdfda298937d0071f95
Kyle <kyle@nonpolynomial.com>
2011-12-12 11:39:39 -0800

    Renamed b2g-dialer-daemon to rilproxy Cleaned up repo a bit

--- Android.mk
+++ Android.mk
@@ -1,17 +1,8 @@
-# Copyright 2006 The Android Open Source Project
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= src/b2g-dialer-daemon.c
+LOCAL_SRC_FILES:= src/rilproxy.c
 LOCAL_SHARED_LIBRARIES := libcutils liblog
-LOCAL_MODULE:= b2g-dialer-daemon
+LOCAL_MODULE:= rilproxy
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_EXECUTABLE)
-# include $(CLEAR_VARS)
-# LOCAL_SRC_FILES:= src/b2g-dialer-forward.cpp
-# LOCAL_SHARED_LIBRARIES := libcutils liblog libmedia libutils libbinder
-# LOCAL_MODULE:= b2g-dialer-forward
-# LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
-# LOCAL_MODULE_TAGS := eng
-# include $(BUILD_EXECUTABLE)
